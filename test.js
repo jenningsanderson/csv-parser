2f5290d0b40ab9f9b0faec7ca3d0a2bf183497af
Jennings Anderson <jennings.anderson@colorado.edu>
2015-01-18 18:42:04 -0700

    first commit on this new branch

--- test.js
+++ test.js
@@ -2,36 +2,18 @@
 var csv = require('./lib/csv.js')
 
 var smallData 	= './sample_data/sample.csv'
-var BigData 	= './sample_data/Fielding.csv'
+var bigData 	= './sample_data/Fielding.csv'
 
 
-//Synchronous Parsing
-// var syncR = csv.parseSync(smallData)
-// console.log(syncR)
+// Synchronous Parsing
+csv.parseSync(smallData)
 
 //ASynchronous Parsing
-var aSyncR = csv.parseAsync(smallData)
-// console.log(aSyncR)
+csv.parseAsync(smallData)
 
-// csv.parseAsync(url, function(error, data){
-// 	if (error)
-// 		throw error
-// 	console.log(data)	
-// })
+//Piped Parsing
+csv.parsePipe(smallData)
 
-// fs.createReadStream('path/to/data.csv')
-//  .pipe(csv.parsePipe())
-//  .pipe(fs.createWriteStream('path/to/data.txt'))
-
-// var p = csv.parseObject(url)
-// p.on('data', function(data){
-// 	console.log('got some data:' + data)
-// })
-// p.on('end', function(){
-// 	console.log('no more data')
-// })
-// p.on('error', function(err){
-// 	alert('something error' + err)
-// })
-
-// p.run()
+//Object Parsing
+var p = csv.buildParser()
+p.run(smallData)
